0c344cd233e1fad0db7c5c3f03ef5b3debef7edc
Xianhui311 <theaxluo@gmail.com>
2021-10-21 17:27:26 -0500

    Created test teleop to be inline with the new drivetrain. Also we are no longer using road runner as we can emulate its functionality without needing to deal with its complexity. More customizability for ourselves

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "13 Drive Train Test", group = "15118")
+public class Test extends LinearOpMode {
+    DcMotor fr, fl, br, bl;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            if (gamepad1.left_stick_x != 0 || gamepad1.right_stick_x != 0 || gamepad1.left_stick_y != 0)
+            {
+                move(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_stick_y);
+            }
+            if(gamepad1.left_stick_x == 0 || gamepad1.left_stick_y == 0 || gamepad1.right_stick_x == 0)
+            {
+                move(0, 0, 0);
+            }
+        }
+    }
+
+    public void initialize()
+    {
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+    }
+
+    public void move(double strafe, double forward, double turn)
+    {
+        //Following is just there if your robot is moving backwards, if it is then just uncomment it
+        forward *= -1;
+        turn *= -1;
+        strafe *= -1;
+        fl.setPower(forward + turn + strafe);
+        fr.setPower(forward - turn + strafe);
+        bl.setPower(forward + turn - strafe);
+        br.setPower(forward - turn - strafe);
+    }
+}
